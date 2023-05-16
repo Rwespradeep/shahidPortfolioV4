258630ce9d010a4864b59a1fce723ae87a65d2dd
Pradeep rvs <37183050+Rwespradeep@users.noreply.github.com>
2023-05-16 11:53:37 +0530

    changed server url

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: "http://localhost:9000",
-    baseURL: "https://shahid-website-server-rwespradeep.vercel.app"
+    //baseURL: "http://localhost:9000",
+    baseURL: "https://shahid-website-server.vercel.app/api"
 });
 
 export default instance;
