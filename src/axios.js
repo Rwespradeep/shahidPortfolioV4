a0b2bff5c83f6177c7fcd99bff1b16f016186201
Pradeep rvs <37183050+Rwespradeep@users.noreply.github.com>
2023-05-16 11:58:08 +0530

    uri changed again, removed /api

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: "http://localhost:9000",
-    baseURL: "https://shahid-website-server.vercel.app/api"
+    baseURL: "https://shahid-website-server.vercel.app"
 });
 
 export default instance;
